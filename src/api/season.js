8aa83d9f030ac1d13c0e98e8f9c15bcd0976b9ee
Your Name <trong.pham@kiaisoft.com>
2023-10-24 13:14:13 +0700

    feat: continue integrate api season

--- src/api/season.js
+++ src/api/season.js
@@ -1,9 +1,10 @@
 import request from '@/utils/request'
 
-export function createSeason() {
+export function createSeason(data) {
   return request({
     url: '/season',
-    method: 'post'
+    method: 'post',
+    data
   })
 }
 
